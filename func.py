d0847194b7ebcc6fc978c1c0f633a0b8d6fb5b3d
Meir Leviev <meirleviev@gmail.com>
2026-03-22 20:33:39 +0200

    Add funcs with pass

---
+++ func.py
@@ -0,0 +1,34 @@
+
+
+def create_matrix(size):
+    pass
+
+def place_mines(num):
+    pass
+
+def numbers():
+    pass
+
+def cover_matrix():
+    pass
+
+def flag_mine():
+    pass
+
+def player_action():
+    pass
+
+def check_square():
+    pass
+
+def stepped_on_a_mine():
+    pass
+
+def check_flags():
+    pass
+
+def win_game():
+    pass
+
+def lose_game():
+    pass
