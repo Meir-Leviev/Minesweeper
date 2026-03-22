6708982fdf5f02cb08cacf31d179b80414d0c9ac
yosef <y7440782@gmail.com>
2026-03-22 21:42:46 +0200

    Add place_mines func

--- func.py
+++ func.py
@@ -1,10 +1,27 @@
-
+import random
 
 def create_matrix(size)->list[list]:
     return [['' for _ in range(size)] for _ in range(size)]
 
-def place_mines(num):
-    pass
+def place_mines(matrix, num_mines):
+    rows = len(matrix)
+    cols = len(matrix[0])
+    total_cells = rows * cols
+
+    if num_mines >= total_cells:
+        num_mines = total_cells - 1
+
+    mines_placed = 0
+    while mines_placed < num_mines:
+        r = random.randint(0, rows - 1)
+        c = random.randint(0, cols - 1)
+
+        if matrix[r][c] != "M":
+            matrix[r][c] = "M"
+            mines_placed += 1
+
+    return matrix
+
 
 def numbers():
     pass
